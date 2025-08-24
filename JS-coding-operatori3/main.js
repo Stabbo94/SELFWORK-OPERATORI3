725d546768f7d44b6984eb4f93f8556decfbd16e
Stabbo94 <albertostaboli@gmail.com>
2025-08-24 21:56:20 +0200

    ALLEGO ESERCIZIO GRAZIE, ALBERTO

---
+++ JS-coding-operatori3/main.js
@@ -0,0 +1,40 @@
+let num1 = 12;
+let num2 = 2;
+
+//Somma
+let sum = num1+num2;
+console.log(sum);
+
+//Sottrazione
+let sottrazione = num1-num2;
+console.log(sottrazione);
+
+//Moltiplicazione
+let moltiplica = num1*num2;
+console.log(moltiplica);
+
+//Divisione
+let divisione = num1/num2;
+console.log(divisione);
+
+//Potenza
+let potenza = num1**num2;
+console.log(potenza);
+
+//Dichiarazione
+console.log(`Con i numeri da te scelti, i risultati delle varie operazioni sono: Somma (${sum}), Sottrazione (${sottrazione}), Moltiplicazione (${moltiplica}), Divisione (${divisione}) ed infine Potenza (${potenza})`);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
